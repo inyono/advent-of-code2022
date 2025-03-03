551e41460ce56954d9016e90282f6b788da1b48c
Jonas Keinholz <git@inyono.dev>
2025-03-03 23:34:47 +0100

    Day2: Various refactorings

--- src/Day02.java
+++ src/Day02.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @see <a href="https://adventofcode.com/2022/day/2">Day 2: Rock Paper Scissors</a>
@@ -16,12 +14,7 @@ public class Day02 {
             'Y', Sign.PAPER,
             'Z', Sign.SCISSORS
     );
-    protected static final Map<Sign, Integer> signToScore = Map.of(
-            Sign.ROCK, 1,
-            Sign.PAPER, 2,
-            Sign.SCISSORS, 3
-    );
-    protected final ArrayList<Input> inputs;
+    protected final List<Input> inputs;
 
     public Day02(Readable input) {
         this.inputs = parseMatches(input);
@@ -32,12 +25,8 @@ public class Day02 {
         for (Input input : inputs) {
             Sign opponentSign = opponentToSign.get(input.opponent);
             Sign ownSign = ownToSign.get(input.own);
-            result += signToScore.get(ownSign);
-            if (isDraw(opponentSign, ownSign)) {
-                result += 3;
-            } else if (isWin(opponentSign, ownSign)) {
-                result += 6;
-            }
+            result += ownSign.score;
+            result += determineResult(opponentSign, ownSign).score;
         }
         return result;
     }
@@ -46,34 +35,61 @@ public class Day02 {
         int result = 0;
         for (Input input : inputs) {
             Sign opponentSign = opponentToSign.get(input.opponent);
-            switch (input.own) {
-                // Lose
-                case 'X' -> {
-                    Sign ownSign = switch (opponentSign) {
-                        case ROCK -> Sign.SCISSORS;
-                        case PAPER -> Sign.ROCK;
-                        case SCISSORS -> Sign.PAPER;
-                    };
-                    result += signToScore.get(ownSign);
-                }
-                // Draw
-                case 'Y' -> {
-                    result += 3;
-                    result += signToScore.get(opponentSign);
-                }
-                // Win
-                case 'Z' -> {
-                    result += 6;
-                    Sign ownSign = switch (opponentSign) {
-                        case ROCK -> Sign.PAPER;
-                        case PAPER -> Sign.SCISSORS;
-                        case SCISSORS -> Sign.ROCK;
-                    };
-                    result += signToScore.get(ownSign);
-                }
-            }
+            Result desiredResult = toResult(input.own);
+            Sign ownSign = determineSignForResult(opponentSign, desiredResult);
+            result += desiredResult.score;
+            result += ownSign.score;
         }
         return result;
+    }
+
+    protected List<Input> parseMatches(Readable input) {
+        try (Scanner scanner = new Scanner(input)) {
+            ArrayList<Input> result = new ArrayList<>();
+            while (scanner.hasNext()) {
+                Character opponent = scanner.next().charAt(0);
+                Character own = scanner.next().charAt(0);
+                result.add(new Input(opponent, own));
+            }
+            return Collections.unmodifiableList(result);
+        }
+    }
+
+    protected Result toResult(Character ch) {
+        return switch (ch) {
+            case 'X' -> Result.LOSE;
+            case 'Y' -> Result.DRAW;
+            case 'Z' -> Result.WIN;
+            default -> throw new IllegalArgumentException("Invalid character: " + ch);
+        };
+    }
+
+    protected Result determineResult(Sign opponentSign, Sign ownSign) {
+        if (isDraw(opponentSign, ownSign)) {
+            return Result.DRAW;
+        } else if (isWin(opponentSign, ownSign)) {
+            return Result.WIN;
+        } else {
+            return Result.LOSE;
+        }
+    }
+
+    protected Sign determineSignForResult(Sign opponentSign, Result desiredResult) {
+        if (desiredResult == Result.DRAW) return opponentSign;
+
+        if (desiredResult == Result.WIN) {
+            return switch (opponentSign) {
+                case ROCK -> Sign.PAPER;
+                case PAPER -> Sign.SCISSORS;
+                case SCISSORS -> Sign.ROCK;
+            };
+        } else {
+            return switch (opponentSign) {
+                case ROCK -> Sign.SCISSORS;
+                case PAPER -> Sign.ROCK;
+                case SCISSORS -> Sign.PAPER;
+            };
+        }
     }
 
     protected boolean isDraw(Sign opponentSign, Sign ownSign) {
@@ -88,19 +104,25 @@ public class Day02 {
         );
     }
 
-    protected ArrayList<Input> parseMatches(Readable input) {
-        Scanner scanner = new Scanner(input);
-        ArrayList<Input> result = new ArrayList<>();
-        while (scanner.hasNext()) {
-            Character opponent = scanner.next().charAt(0);
-            Character own = scanner.next().charAt(0);
-            result.add(new Input(opponent, own));
-        }
-        return result;
-    }
 
     protected enum Sign {
-        ROCK, PAPER, SCISSORS
+        ROCK(1), PAPER(2), SCISSORS(3);
+
+        public final int score;
+
+        Sign(int score) {
+            this.score = score;
+        }
+    }
+
+    protected enum Result {
+        WIN(6), DRAW(3), LOSE(0);
+
+        public final int score;
+
+        Result(int score) {
+            this.score = score;
+        }
     }
 
     protected record Input(Character opponent, Character own) {
